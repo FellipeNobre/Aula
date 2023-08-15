78cb56d67bc6a00bba2dd928a837eb5b1ec0bfb9
FELLIPE NOBRE BIZARRIA <fellipe.nbizarria@senacsp.edu.br>
2023-08-14 21:44:50 -0300

    Finalizado

--- src/main/java/exemplos/AviaoSenac.java
+++ src/main/java/exemplos/AviaoSenac.java
@@ -3,11 +3,39 @@ package exemplos;
 
 public class AviaoSenac {
     
-    String cor;
+    private String cor;
     String marca = "A101";
     int velocidade;
     double altitude;
     boolean ligado = false;
+    
+    public AviaoSenac(){
+        this.cor = "Branca";
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
     
     //Ações = metodos
     
@@ -21,4 +49,11 @@ public class AviaoSenac {
         return velocidade;
     }
     
+    public void setCor(String novaCor){
+        this.cor = novaCor.toUpperCase();
+    }
+    
+    public String getCor(){
+        return this.cor.toUpperCase();
+    }
 }
--- src/main/java/exemplos/Fabrica.java
+++ src/main/java/exemplos/Fabrica.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package exemplos;
 
-/**
- *
- * @author fellipe.nbizarria
- */
 public class Fabrica {
     
+    public static void main(String[] args){
+        
+        AviaoSenac meuAviao1 = new AviaoSenac();
+        System.out.println("cor: " + meuAviao1.getCor() );
+        
+        AviaoSenac novoAviao = new AviaoSenac();
+        novoAviao.setCor("vermelho");
+        
+        System.out.println(novoAviao.getCor() );
+        
+       novoAviao.acelerar(0);
+       System.out.println("Aviao - novoAviao - velocidade:" + novoAviao.getVelocidade());
+    }
 }
